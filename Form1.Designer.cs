aaa77607d07a84e1cb492528e0e963ceff2515f9
jussivirkkala <jussivirkkala@hotmail.com>
2020-11-11 14:32:59 +0200

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace HIK_Set
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (m_lUserID >= 0)
+            {
+                CHCNetSDK.NET_DVR_Logout(m_lUserID);
+            }
+            if (m_bInitSDK == true)
+            {
+                CHCNetSDK.NET_DVR_Cleanup();
+            }
+            if (disposing)
+            {
+                if (components != null)
+                {
+                    components.Dispose();
+                }
+            }
+            base.Dispose(disposing);
+
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(12, 23);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(72, 29);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "&Day";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(115, 23);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(82, 29);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.Text = "&Night";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(230, 23);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(78, 29);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.Text = "&Auto";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 25);
+            this.label1.TabIndex = 3;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Checked = true;
+            this.radioButton4.Location = new System.Drawing.Point(356, 23);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(76, 29);
+            this.radioButton4.TabIndex = 0;
+            this.radioButton4.Text = "Test";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(334, 109);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton radioButton4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/**
+ * @jussivirkkala
+ * 2020-11-10 1.0.1 
+ * 2020-11-08 1.0.0 .NET 4.5.2, allow unsafe code, Platform x64
+ * 
+ */
+
+
+namespace HIK_Set
+{
+    public partial class Form1 : Form
+    {
+        // On top
+        static readonly IntPtr HWND_TOPMOST = new IntPtr(-1);
+        static readonly IntPtr HWND_NOTOPMOST = new IntPtr(-2);
+        static readonly IntPtr HWND_TOP = new IntPtr(0);
+        static readonly IntPtr HWND_BOTTOM = new IntPtr(1);
+        const UInt32 SWP_NOSIZE = 0x0001;
+        const UInt32 SWP_NOMOVE = 0x0002;
+        const UInt32 TOPMOST_FLAGS = SWP_NOMOVE | SWP_NOSIZE;
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
+
+        private Int32 m_lUserID = -1;
+        private bool m_bInitSDK = false;
+        public int PreSetNo = 1;
+
+        CHCNetSDK.REALDATACALLBACK RealData = null;
+        public CHCNetSDK.NET_DVR_PTZPOS m_struPtzCfg;
+
+     //   private System.ComponentModel.Container components = null;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+             m_bInitSDK = CHCNetSDK.NET_DVR_Init();
+            if (m_bInitSDK == false)
+            {
+                label1.Text = "SDK error";
+            }
+        }
+
+        [STAThread]
+        static void Main()
+        {
+            Application.Run(new Form1());
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SetWindowPos(this.Handle, HWND_TOPMOST, 0, 0, 0, 0, TOPMOST_FLAGS);
+            this.Text = System.Diagnostics.Process.GetCurrentProcess().ProcessName; // + " " + Application.ProductVersion; //  FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location).FileVersion; //  . Assembly.GetEntryAssembly().GetName().Version; //. System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString();
+            
+            radioButton3.Checked = true;
+            this.FormClosing += new FormClosingEventHandler(Form1_Closing);
+
+        }
+
+       
+        private void Form1_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            PreSetNo = 46;
+            SendPreset();
+
+            //MessageBox.Show("a");
+            //e.Cancel = true;
+        }
+
+
+            private void SendPreset()
+        {
+            label1.Text = "";
+            label1.Text += "Cam1";
+            radioButton1.Enabled = false;
+            radioButton2.Enabled = false;
+            radioButton3.Enabled = false;
+            string DVRIPAddress;
+            DVRIPAddress = "192.168.106.5";
+            Preset(DVRIPAddress);
+            label1.Text += " Cam2";
+            DVRIPAddress = "192.168.106.6";
+            Preset(DVRIPAddress);
+            radioButton1.Enabled = true;
+            radioButton2.Enabled = true;
+            radioButton3.Enabled = true;
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            RadioButton rb = sender as RadioButton;
+            if (rb.Checked)
+            {      PreSetNo = 39;
+            SendPreset();
+    //            PreSetNo = 46;
+      //          SendPreset();
+            //    radioButton3.Checked = true;
+
+            }
+        }
+
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            RadioButton rb = sender as RadioButton;
+            if (rb.Checked)
+        { 
+                PreSetNo = 40;
+                SendPreset();
+//                PreSetNo = 46;
+  //              SendPreset();
+              //  radioButton3.Checked = true;
+            }
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            RadioButton rb = sender as RadioButton;
+                if (rb.Checked)
+            {
+                PreSetNo = 46;
+                SendPreset();
+            }
+        }
+
+        private void Preset(string DVRIPAddress)
+        { 
+            Int16 DVRPortNumber = Int16.Parse("8000");
+            string DVRUserName = "admin";
+            string DVRPassword = "!kv14ST2$";
+
+            CHCNetSDK.NET_DVR_DEVICEINFO_V30 DeviceInfo = new CHCNetSDK.NET_DVR_DEVICEINFO_V30();
+
+            //µÇÂ¼Éè±¸ Login the device
+            m_lUserID = CHCNetSDK.NET_DVR_Login_V30(DVRIPAddress, DVRPortNumber, DVRUserName, DVRPassword, ref DeviceInfo);
+            if (m_lUserID < 0)
+            {
+                label1.Text += ", login err";
+            }
+            else
+            {
+                if (!CHCNetSDK.NET_DVR_PTZPreset_Other(m_lUserID, 1, CHCNetSDK.GOTO_PRESET, (UInt32)(PreSetNo)))
+                    label1.Text += ", set err";
+                else
+                {
+                    label1.Text += ", ok";
+               //     PreSetNo = 46;
+                 //   if (!CHCNetSDK.NET_DVR_PTZPreset_Other(m_lUserID, 1, CHCNetSDK.GOTO_PRESET, (UInt32)(PreSetNo)))
+                   //     label1.Text += ", 46";
+
+                }
+
+                if (!CHCNetSDK.NET_DVR_Logout(m_lUserID))
+                {
+                    label1.Text += ", logout err";
+                    m_lUserID = -1;
+                }
+            }
+        }
+
+    }
+}
