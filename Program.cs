26de44f6331069ad082bab1980b605ddb339caf1
jussivirkkala <jussivirkkala@hotmail.com>
2020-11-12 14:38:06 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -8,12 +9,24 @@ namespace HIK_Set
 {
     static class Program
     {
+        private static Mutex mutex = null;
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
+
+            string appName = System.Diagnostics.Process.GetCurrentProcess().ProcessName;
+            bool createdNew;
+            mutex = new Mutex(true, appName, out createdNew);
+            if (!createdNew)
+            {
+                MessageBox.Show("Already running",appName);
+                //app is already running! Exiting the application  
+                return;
+            }
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
