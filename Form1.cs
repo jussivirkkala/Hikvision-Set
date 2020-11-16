339de1e2c5f0e24af113dab44a89d513f90bf5b2
jussivirkkala <jussivirkkala@hotmail.com>
2020-11-16 16:17:07 +0200

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -14,11 +14,14 @@ using System.Windows.Forms;
 
 /**
  * @jussivirkkala
- * 2020-11-13 Remove ..\bin from CHCNetSDK
+ * https://www.hikvision.com/en/support/download/sdk/device-network-sdk--for-windows-64-bit-/
+ * V6.1.6.3_build20200925
+ * 
+ * 2020-11-13 1.1.0 Removed ..\bin from CHCNetSDK, reading .ini, writing .log
  * 2020-11-12 1.0.3 Application already running
  * 2020-11-10 1.0.2 opacity
  * 2020-11-10 1.0.1 git
- * 2020-11-08 1.0.0 .NET 4.5.2, allow unsafe code, Platform x64
+ * 2020-11-08 1.0.0 .NET 4.5, allow unsafe code, Platform x64
  * 
  */
 
@@ -41,9 +44,21 @@ namespace HIK_Set
         [return: MarshalAs(UnmanagedType.Bool)]
         public static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
 
+        struct Camera
+        {
+            public string DVRIPAddress;
+            public Int16 DVRPortNumber;
+            public string DVRUserName;
+            public string DVRPassword;
+        }
+
+        Camera Camera1;
+        Camera Camera2;
+
+
+        // HIKVision
         private Int32 m_lUserID = -1;
         private bool m_bInitSDK = false;
-        public int PreSetNo = 1;
 
         CHCNetSDK.REALDATACALLBACK RealData = null;
         public CHCNetSDK.NET_DVR_PTZPOS m_struPtzCfg;
@@ -58,7 +73,6 @@ namespace HIK_Set
             }
         }
 
-
         [STAThread]
         static void Main()
         {
@@ -66,151 +80,157 @@ namespace HIK_Set
         }
 
 
-//        https://docs.microsoft.com/en-us/dotnet/standard/serialization/system-text-json-how-to?pivots=dotnet-5-0
+        string appName;
 
         private void Form1_Load(object sender, EventArgs e)
         {
             // Always on top
             SetWindowPos(this.Handle, HWND_TOPMOST, 0, 0, 0, 0, TOPMOST_FLAGS);
-            string appName = System.Diagnostics.Process.GetCurrentProcess().ProcessName;
+            appName = System.Diagnostics.Process.GetCurrentProcess().ProcessName;
             this.Text = appName;
             this.Opacity = .95;
+            Log("Opening");
 
             // On closing event
             this.FormClosing += new FormClosingEventHandler(Form1_Closing);
-
 
             // Load file
             if (!File.Exists(this.Text + ".ini"))
             {
                 MessageBox.Show("Missing "+appName+".ini", appName);
-                }
-                else
+            }
+            else
+            {
+                int row = 0;
+                foreach (string line in File.ReadLines(this.Text + ".ini"))
                 {
-                    foreach (string line in File.ReadLines(this.Text + ".ini"))
+                    if (!line.StartsWith("#"))
                     {
-                        if (!line.StartsWith("#"))
+                        row += 1;
+                        switch (row)
                         {
-                            MessageBox.Show(line);
+                        case 1:
+                            Camera1.DVRIPAddress = line;
+                            break;
+                        case 2:
+                            Camera1.DVRPortNumber = Int16.Parse(line);
+                            break;
+                        case 3:
+                            Camera1.DVRUserName= line;
+                            break;
+                        case 4:
+                            Camera1.DVRPassword= line;
+                            break;
+                        case 5:
+                            Camera2.DVRIPAddress = line;
+                            break;
+                        case 6:
+                            Camera2.DVRPortNumber = Int16.Parse(line);
+                            break;
+                        case 7:
+                            Camera2.DVRUserName = line;
+                            break;
+                        case 8:
+                            Camera2.DVRPassword = line;
+                            break;
                         }
                     }
-
                 }
-            
 
+            }
         }
-
 
         private void Form1_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
-  //          PreSetNo = 46;
-//            SendPreset();
             radioButton3.Checked = true;
-            //MessageBox.Show("a");
-            //e.Cancel = true;
+            Log("Closing");
         }
 
-        
-            private void SendPreset()
+
+        private void SendPreset(UInt32 Code)
         {
             label1.Text = "";
-            label1.Text += "Cam1:";
             radioButton1.Enabled = false;
             radioButton2.Enabled = false;
             radioButton3.Enabled = false;
-            string DVRIPAddress;
-            DVRIPAddress = "192.168.106.5";
-            Preset(DVRIPAddress);
-            label1.Text += " Cam2:";
-            DVRIPAddress = "192.168.106.6";
-            Preset(DVRIPAddress);
+            if (Camera1.DVRPortNumber > 0)
+            {
+                Log("Camera1");
+                label1.Text += "Cam1: ";
+                Log(Code.ToString());
+                Preset(Camera1, Code);
+            }
+            if (Camera2.DVRPortNumber > 0)
+            {
+                Log("Camera2");
+                label1.Text += " Cam2: ";
+                Log(Code.ToString());
+                Preset(Camera2, Code);
+            }
             radioButton1.Enabled = true;
             radioButton2.Enabled = true;
             radioButton3.Enabled = true;
-
         }
 
+
+        // Day
         private void radioButton1_CheckedChanged(object sender, EventArgs e)
         {
             RadioButton rb = sender as RadioButton;
             if (rb.Checked)
-            {      PreSetNo = 39;
-            SendPreset();
-    //            PreSetNo = 46;
-      //          SendPreset();
-            //    radioButton3.Checked = true;
-
-            }
+                SendPreset(39);
         }
-
-
+        // Night
         private void radioButton2_CheckedChanged(object sender, EventArgs e)
         {
             RadioButton rb = sender as RadioButton;
             if (rb.Checked)
-        { 
-                PreSetNo = 40;
-                SendPreset();
-//                PreSetNo = 46;
-  //              SendPreset();
-              //  radioButton3.Checked = true;
-            }
+                SendPreset(40);
         }
-
+        // Auto
         private void radioButton3_CheckedChanged(object sender, EventArgs e)
         {
             RadioButton rb = sender as RadioButton;
-                if (rb.Checked)
-            {
-                PreSetNo = 46;
-                SendPreset();
-            }
+            if (rb.Checked)
+                SendPreset(46);
         }
 
         void Log(string s)
         {
             try { 
-            using (StreamWriter sw = File.AppendText("log.txt"))
-            {
-                sw.WriteLine(DateTime.Now.ToString("yyyy-MM-dd-THH:mm:ss.fff") + DateTime.Now.ToString("zzz") + "\t" + Environment.MachineName+"\t"+s);
-            }
+                using (StreamWriter sw = File.AppendText(appName+".log"))
+                {
+                    sw.WriteLine(DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ss.fff") + DateTime.Now.ToString("zzz") + "\t" + Environment.MachineName+"\t"+Environment.UserName+"\t"+"\t"+s);
+                }
             }
             catch
             { }
 
         }
-        private void Preset(string DVRIPAddress)
+        private void Preset(Camera Camera, UInt32 PreSetNo)
         { 
-            Int16 DVRPortNumber = Int16.Parse("8000");
-            string DVRUserName = "admin";
-            string DVRPassword = "!kv14ST2$";
-
             CHCNetSDK.NET_DVR_DEVICEINFO_V30 DeviceInfo = new CHCNetSDK.NET_DVR_DEVICEINFO_V30();
 
-            //µÇÂ¼Éè±¸ Login the device
-            m_lUserID = CHCNetSDK.NET_DVR_Login_V30(DVRIPAddress, DVRPortNumber, DVRUserName, DVRPassword, ref DeviceInfo);
+            m_lUserID = CHCNetSDK.NET_DVR_Login_V30(Camera.DVRIPAddress, Camera.DVRPortNumber, Camera.DVRUserName, Camera.DVRPassword, ref DeviceInfo);
             if (m_lUserID < 0)
             {
-                label1.Text += " login err";
+                label1.Text += "login err";
                 Log("login err");
             }
             else
             {
                 if (!CHCNetSDK.NET_DVR_PTZPreset_Other(m_lUserID, 1, CHCNetSDK.GOTO_PRESET, (UInt32)(PreSetNo)))
-                    label1.Text += " set err";
-                else
                 {
-                    label1.Text += " ok";
-               //     PreSetNo = 46;
-                 //   if (!CHCNetSDK.NET_DVR_PTZPreset_Other(m_lUserID, 1, CHCNetSDK.GOTO_PRESET, (UInt32)(PreSetNo)))
-                   //     label1.Text += ", 46";
-
-
+                    label1.Text += "set err";
+                    Log("set err");
                 }
+                else
+                    label1.Text += "ok";
 
                 if (!CHCNetSDK.NET_DVR_Logout(m_lUserID))
                 {
-                    label1.Text += ", logout err";
+                    label1.Text += " logout err";
+                    Log("logout err");
                     m_lUserID = -1;
                 }
             }
