43bcde01606a4ad8b58d41377f158acda1859cc1
jussivirkkala <jussivirkkala@hotmail.com>
2020-11-18 18:32:38 +0200

    Update AssemblyInfo.cs

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,8 +5,8 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("HIK-Set")]
-[assembly: AssemblyDescription("Sending preset commands to 1/2 HIKVISION camera")]
+[assembly: AssemblyTitle("Hik-Set")]
+[assembly: AssemblyDescription("Sending preset commands to 1-2 Hikvsion camera")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("HIK-Set")]
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.2.0")]
